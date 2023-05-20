991a5e80e780f27dca3d6fee3b271e741d58ecab
Deivys Larez <larezdeivys20@gmail.com>
2023-05-20 10:48:11 -0400

    first commit

---
+++ js/desplegable.js
@@ -0,0 +1,4 @@
+document.querySelector('.navbar .img2').addEventListener('click', () => {
+	document.querySelector('.enlaces').classList.toggle('enlaces-ul')
+	document.querySelector('.enlaces ul').classList.toggle('enlaces-ul');
+})
---
+++ js/switch.js
@@ -0,0 +1,6 @@
+
+		document.querySelector('.semasfo').addEventListener('click', (e) => {
+		if(e.target.classList.contains('switch-on')){
+		e.target.classList.toggle('switch-off')
+	}
+})
